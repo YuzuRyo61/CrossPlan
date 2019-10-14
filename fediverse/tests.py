b567426be316cd255f4986973aeb60187edad1ca
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-10-14 12:41:45 +0900

    new assert tests

--- fediverse/tests.py
+++ fediverse/tests.py
@@ -10,5 +10,9 @@ class APResponseTests(TestCase):
     def test_response_person(self):
         testingUser = User.objects.create_user("testUser")
         res = self.client.get(reverse('UserShow', kwargs={'username': testingUser.username}), HTTP_ACCEPT="application/activity+json")
-        self.assertEqual(type(json.loads(res.content.decode('utf-8'))), dict, "Should response is json(ActivityPub)")
-        
+        resDict = json.loads(res.content.decode('utf-8'))
+        self.assertEqual(type(resDict), dict, "Should response is json(ActivityPub)")
+        self.assertEqual(type(resDict.get('@context')), list, "Should @context is list")
+        self.assertEqual(resDict.get('type'), "Person", "Should user is not bot(Should return Person)")
+        self.assertEqual(resDict.get('preferredUsername'), "testUser", "preferredUsername is username")
+        self.assertEqual(type(resDict.get('publicKey')), dict, "Should publicKey is dict")
