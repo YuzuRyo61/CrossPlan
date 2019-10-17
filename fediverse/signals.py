b6c96278279479131bd4fa40a131cb5ddb44292f
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-10-17 19:13:24 +0900

    define model signal

--- fediverse/signals.py
+++ fediverse/signals.py
@@ -1,11 +1,15 @@
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
-from .models import Post
+from .models import Post, Like
 
 @receiver(post_save, sender=Post)
 def savePostSignal(sender, instance, created, **kwargs):
     print("Post signal")
+
+@receiver(post_save, sender=Like)
+def savePostLike(sender, instance, created, **kwargs):
+    print("liked")
 
 @receiver(pre_delete, sender=Post)
 def preDeletePostSignal(sender, instance, using, **kwargs):
