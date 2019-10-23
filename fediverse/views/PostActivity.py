9c9ee31b635abcfcc08b58e79251f87b840d9d8d
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-10-23 09:11:52 +0900

    Post Activity

--- fediverse/views/PostActivity.py
+++ fediverse/views/PostActivity.py
@@ -1,4 +1,23 @@
+from django.shortcuts import get_object_or_404
 
+from fediverse.views.renderer.response import APResponse
+from fediverse.views.renderer.head import APRender
+from fediverse.views.renderer.activity.Create import RenderCreate
+from fediverse.views.renderer.object.Note import RenderNote
+
+from fediverse.models import Post
 
 def PostActivity(request, uuid):
-    pass
+    postObj = get_object_or_404(Post, uuid=uuid)
+    return APResponse(APRender(RenderCreate(
+        postObj.uuid,
+        postObj.parent.username,
+        RenderNote(
+            postObj.uuid,
+            postObj.posted.isoformat(),
+            postObj.parent.username,
+            postObj.body,
+            ["https://www.w3.org/ns/activitystreams#Public"],
+            [f"https://{settings.CP_ENDPOINT}{reverse('Fediverse:Followers', kwargs={'username': postObj.parent.username})}"]
+        )
+    )))
