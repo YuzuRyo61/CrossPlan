536e589889c33d86aa03881bd715ba80c3ab6c92
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-11-20 08:26:17 +0900

    fix script in create activity

--- fediverse/views/inboxProcess/Create.py
+++ fediverse/views/inboxProcess/Create.py
@@ -13,7 +13,7 @@ from fediverse.views.renderer.activity.Accept import RenderAccept
 from fediverse.views.renderer.activity.Reject import RenderReject
 
 def _CreateActivity(body, fromUserObj):
-    try:
+    if Post.objects.filter(fediID=body["object"]["id"]).count() == 0: # pylint: disable=no-member
         newPost = Post(
             fediID=body["object"]["id"],
             body=body["object"]["content"],
@@ -21,6 +21,6 @@ def _CreateActivity(body, fromUserObj):
             posted=parse(body["object"]["published"])
         )
         newPost.save()
-    except (IntegrityError, ):
-        logging.warn("This post is already created. Skippking.")
+    else:
+        logging.warn("This post is already created. Skipping.")
     return HttpResponse(status=202)
