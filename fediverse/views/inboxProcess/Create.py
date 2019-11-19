b7732c01ad8938b2deae3c4b88027e7d655de2fe
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-11-19 15:31:46 +0900

    skip create if created fediverse post

--- fediverse/views/inboxProcess/Create.py
+++ fediverse/views/inboxProcess/Create.py
@@ -1,6 +1,9 @@
+import logging
+
 from dateutil.parser import parse
 
 from django.http.response import HttpResponse
+from django.db.utils import IntegrityError
 
 from CrossPlan.tasks import APSend
 
@@ -10,11 +13,14 @@ from fediverse.views.renderer.activity.Accept import RenderAccept
 from fediverse.views.renderer.activity.Reject import RenderReject
 
 def _CreateActivity(body, fromUserObj):
-    newPost = Post(
-        fediID=body["object"]["id"],
-        body=body["object"]["content"],
-        parentFedi=fromUserObj,
-        posted=parse(body["object"]["published"])
-    )
-    newPost.save()
+    try:
+        newPost = Post(
+            fediID=body["object"]["id"],
+            body=body["object"]["content"],
+            parentFedi=fromUserObj,
+            posted=parse(body["object"]["published"])
+        )
+        newPost.save()
+    except (IntegrityError, ):
+        logging.warn("This post is already created. Skippking.")
     return HttpResponse(status=202)
